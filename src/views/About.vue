76a5d0afb6223962c174c5c7f69d1f457856c7c0
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-01-15 01:33:13 +0800

    Remove example files

--- src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-
-    <!-- 放入 Bootstrap Button -->
-    <button type="button" class="btn btn-info">Info</button>
-  </div>
-</template>
