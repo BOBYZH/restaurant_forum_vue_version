96900bf3e0e14a78082e6ad5af9f497dd2637e39
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-01-18 00:23:10 +0800

    Add signIn page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container py-5">
+    <!-- 預防表單自動提交 -->
+    <form class="w-100" @submit.prevent.stop="handleSubmit">
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">
+          Sign In
+        </h1>
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="email">email</label>
+        <input
+          id="email"
+          v-model預防表單自動提交預防表單自動提交
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="username"
+          required
+          autofocus
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="current-password"
+          required
+        >
+      </div>
+
+      <button
+        class="btn btn-lg btn-primary btn-block mb-3"
+        type="submit"
+      >
+        Submit
+      </button>
+
+      <div class="text-center mb-3">
+        <!-- 避免瀏覽器預設的連結行為，只渲染不同的 component -->
+        <router-link to="/signup">
+          Sign Up
+        </router-link>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">
+        &copy; 2017-2021
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit (e) {
+      const data = JSON.stringify({
+        email: this.email,
+        password: this.password
+      })
+
+      // TODO: 向後端驗證使用者登入資訊是否合法
+      console.log('data', data)
+    }
+  }
+}
+</script>
