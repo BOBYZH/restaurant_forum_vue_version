b64a8c32400a23b11c6c515c802776beafc08aa6
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-01-18 17:05:24 +0800

    Add annotation of  Ternary Operator

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -11,6 +11,7 @@
         <h4>
           <a href="#">{{ restaurant.name }}</a>
           &nbsp;
+          <!-- 用三元運算子，避免未分類的資料顯示不出來 -->
           <small>{{ restaurant.Category ? restaurant.Category.name : '未分類'}}</small>
         </h4>
         <p>{{ restaurant.description }}</p>
