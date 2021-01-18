01d7606383feac47d34e01110e537346579c7fd6
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-01-18 16:43:34 +0800

    Add dummyData for NewestRestaurants

---
+++ src/components/NewestRestaurants.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      最新餐廳
+    </div>
+    <div class="card-body">
+      <div
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+      >
+        <h4>
+          <a href="#">{{ restaurant.name }}</a>
+          &nbsp;
+          <small>{{ restaurant.Category ? restaurant.Category.name : '未分類'}}</small>
+        </h4>
+        <p>{{ restaurant.description }}</p>
+        {{ restaurant.createdAt }}
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 加上 props 屬性取得父層資料
+  props: {
+    restaurants: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+small {
+  font-size: 10px;
+}
+</style>
