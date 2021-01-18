1eff2213bd980b5686a0647429c30b68011f0c5b
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-01-18 18:41:27 +0800

    Add v-if/else and methods to buttons

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -18,27 +18,36 @@
         </p>
       </div>
       <div class="card-footer">
+        <!-- 綁定瀏覽器的原生事件，並串上 stop.prevent避免預設動作 -->
         <button
+          v-if="restaurant.isFavorited"
           type="button"
           class="btn btn-danger btn-border favorite mr-2"
+          @click.stop.prevent="deleteFavorite"
         >
           移除最愛
         </button>
         <button
+          v-else
           type="button"
           class="btn btn-primary btn-border favorite mr-2"
+          @click.stop.prevent="addFavorite"
         >
           加到最愛
         </button>
         <button
+          v-if="restaurant.isLiked"
           type="button"
           class="btn btn-danger like mr-2"
+          @click.stop.prevent="deleteLike"
         >
           Unlike
         </button>
         <button
+          v-else
           type="button"
           class="btn btn-primary like mr-2"
+          @click.stop.prevent="addLike"
         >
           Like
         </button>
@@ -58,6 +67,32 @@ export default {
   data () {
     return {
       restaurant: this.initialRestaurant
+    }
+  },
+  methods: {
+    addFavorite () {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isFavorited: true
+      }
+    },
+    deleteFavorite () {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isFavorited: false
+      }
+    },
+    addLike () {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: true
+      }
+    },
+    deleteLike () {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: false
+      }
     }
   }
 }
