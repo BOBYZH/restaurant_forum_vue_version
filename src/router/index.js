f9d8c55954c2df95254130fa3864a60fabec0459
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-02-03 11:25:48 +0800

    Add AdminRestaurants and  AdminRestaurant to router

--- src/router/index.js
+++ src/router/index.js
@@ -75,6 +75,21 @@ routes: [...]
       component: () => import('../views/User.vue')
     },
     {
+      path: '/admin',
+      exact: true,
+      redirect: '/admin/restaurants'
+    },
+    {
+      path: '/admin/restaurants',
+      name: 'admin-restaurants',
+      component: () => import('../views/AdminRestaurants.vue')
+    },
+    {
+      path: '/admin/restaurants/:id',
+      name: 'admin-restaurant',
+      component: () => import('../views/AdminRestaurant.vue')
+    },
+    {
       path: '*',
       name: 'not-found',
       component: NotFound
