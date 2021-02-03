0eacdde83c3830bb9fcba94c681c6bec92768df1
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-02-03 11:24:59 +0800

    Add AdminRestaurants page

---
+++ src/views/AdminRestaurants.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container py-5">
+    <!-- 後台導覽頁籤 AdminNav -->
+    <AdminNav />
+
+    <a
+      href="#"
+      class="btn btn-primary mb-4"
+    >
+      New Restaurant
+    </a>
+
+    <!-- 後台餐廳列表 AdminRestaurantsTable -->
+    <AdminRestaurantsTable />
+  </div>
+</template>
+
+<script>
+import AdminNav from './../components/AdminNav'
+import AdminRestaurantsTable from './../components/AdminRestaurantsTable'
+
+export default {
+  components: {
+    AdminNav,
+    AdminRestaurantsTable
+  }
+}
+</script>
