443a532f2e5dd1011ea582b6e75df92bf5bb6a7a
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-02-07 20:39:32 +0800

    Add UserEdit page

--- src/router/index.js
+++ src/router/index.js
@@ -75,6 +75,11 @@ routes: [...]
       component: () => import('../views/User.vue')
     },
     {
+      path: '/users/:id/edit',
+      name: 'user-edit',
+      component: () => import('../views/UserEdit.vue')
+    },
+    {
       path: '/admin',
       exact: true,
       redirect: '/admin/restaurants'
