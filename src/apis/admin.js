93b4cf5fc9607f45c3df3fe8e9a7a0bfc007905e
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-03-18 23:41:58 +0800

    Update API of AdminRestaurantTable with "get"

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -7,15 +7,22 @@ export default {
       return apiHelper.get('/admin/categories', {
         headers: { Authorization: `Bearer ${getToken()}` }
       })
+    }
+  },
+  restaurants: {
+    get () {
+      return apiHelper.get('/admin/restaurants', {
+        headers: {
+          Authorization: `Bearer ${getToken()}`
+        }
+      })
     },
-    restaurants: {
-      create ({ formData }) {
-        return apiHelper.post('/admin/restaurants', formData, {
-          headers: {
-            Authorization: `Bearer ${getToken()}`
-          }
-        })
-      }
+    create ({ formData }) {
+      return apiHelper.post('/admin/restaurants', formData, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`
+        }
+      })
     }
   }
 }
