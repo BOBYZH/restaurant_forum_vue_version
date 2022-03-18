4fb574f7280cc054e5439c2a096bc6820ee00e2c
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-03-19 00:28:06 +0800

    Add other baseURLs as APIs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'https://forum-express-api.herokuapp.com/api'
+// const baseURL = 'https://forum-express-api.herokuapp.com/api' // AC教案版
+// const baseURL = 'http://localhost:3000/api' // 本地開發版
+const baseURL = 'https://restaurant-forum-sql-practice.herokuapp.com/api' // 個人練習版
 
 export const apiHelper = axios.create({
   baseURL
