e64149008baf8ed2b9d8b2dcb34fae16bc1eb376
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-03-20 18:36:32 +0800

    Add api of "delete" and currentUser to RestaurantComment

--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -36,16 +36,9 @@
 // 載入撰寫好的 mixin
 import { fromNowFilter } from './../utils/mixins'
 
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: '管理者',
-    email: 'root@example.com',
-    image: 'https://i.pravatar.cc/300',
-    isAdmin: true
-  },
-  isAuthenticated: true
-}
+import { mapState } from 'vuex'
+import commentsAPI from './../apis/comments'
+import { Toast } from './../utils/helpers'
 
 export default {
   // 透過 mixins 屬性將撰寫好的 mixin 放入
@@ -56,17 +49,35 @@ export default {
       required: true
     }
   },
-  data () {
-    return {
-      currentUser: dummyUser.currentUser
-    }
+  computed: {
+    ...mapState(['currentUser'])
   },
   methods: {
-    handleDeleteButtonClick (commentId) {
-      console.log('handleDeleteButtonClick', commentId)
-      // TODO: 請求 API 伺服器刪除 id 為 commentId 的評論
-      // 觸發父層事件 - $emit( '事件名稱' , 傳遞的資料 )，通知父元件去更新畫面的狀態
-      this.$emit('after-delete-comment', commentId)
+    async handleDeleteButtonClick (commentId) {
+      try {
+        // 請求 API 伺服器刪除 id 為 commentId 的評論
+        this.isProcessing = true
+        const { data } = await commentsAPI.delete({ commentId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+        // console.log('handleDeleteButtonClick', commentId)
+
+        // 觸發父層事件 - $emit( '事件名稱' , 傳遞的資料 )，通知父元件去更新畫面的狀態
+        this.$emit('after-delete-comment', commentId)
+        Toast.fire({
+          icon: 'success',
+          title: '移除評論成功'
+        })
+        this.isProcessing = false
+      } catch (error) {
+        console.error(error.message)
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法移除評論，請稍後再試'
+        })
+      }
     }
   }
 }
